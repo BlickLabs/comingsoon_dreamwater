7d548744992c1f28436c3f0e2f76562cb532f5f1
Alan Sanchez <sanchezpineda03@gmail.com>
2016-12-24 00:36:07 -0600

    Modified notification styles.

--- js/notifyMe.js
+++ js/notifyMe.js
@@ -16,50 +16,62 @@
 			msgSuccess: "Suscripción realizada con éxito."
 		}, options );
 
-  
+
 		var form = $(this);
 		var input = form.find("input[name=email]");
 		var button = form.find("button");
 		var loader = form.closest(".notify-wrap").children(".loader-container");
-		
+
 		var action = form.attr("action");
-		var message = $("<div class='col-lg-12 align-center' id='message'></div>").appendTo(form);
+		var message = $('<div class="form-message"></div>').appendTo('#contact .main-menu');
+		var messagemobile = $('<div class="col-lg-12 align-center form-message mobilemessage"></div>').appendTo(form);
+		var messages = $('.form-message');
 
 		loader.hide();
-	  
+
 		form.on("submit", function (e) {
 			e.preventDefault();
 			// Test if the value of input is actually an email
 			$.ajax({
 	      type: 'POST',
-	      url: action, 
+	      url: action,
 	      data: form.serialize(),
 	      beforeSend: function () {
 	        var isEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
-	        message.text('');
+	        messages.text('');
 	        if (isEmail.test(input.val())) {
 	          button.attr('disabled', 'disabled');
 	          loader.show();
 	        } else {
-	           message.text(settings.msgErrorFormat);
-	           return false;
+           	messages.text(settings.msgErrorFormat);
+						messages.removeClass('success');
+						messages.addClass('error');
+           	return false;
 	        }
 	      },
 	      success: function (data) {
 	        if (data.status == 'subscribed') {
-	          message.text(settings.msgSuccess);
-	        } else if (data.type == "ValidationError") { 
-						message.text(settings.msgErrorValidation);
+	          messages.text(settings.msgSuccess);
+						messages.removeClass('error');
+						messages.addClass('success');
+	        } else if (data.type == "ValidationError") {
+						messages.text(settings.msgErrorValidation);
+						messages.removeClass('success');
+						messages.addClass('error');
 					} else {
-						message.text(settings.msgError503);
+						messages.text(settings.msgError503);
+						messages.removeClass('success');
+						messages.addClass('error');
 					}
 	      },
 	      error: function (data) {
 	      	if (data.status == 404) {
-	      		message.text(settings.msgError404);
+	      		messages.text(settings.msgError404);
 	      	} else {
-	      		message.text(settings.msgError503);
+	      		messages.text(settings.msgError503);
 	      	}
+					messages.removeClass('success');
+					messages.addClass('error');
 	      },
 	      complete: function () {
 	        button.removeAttr('disabled');
